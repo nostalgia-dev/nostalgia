f735b50dd074b30fc69bdd8b2f291f84a9047246
Nickolay Vasilishin <nikolay_vasilishin@epam.com>
2020-02-16 21:28:05 +0100

    [NSG-010] Create Spotify Datasource
      -- Fixed abnamro, spotify, ns
      -- Added apple applications and icloud usage

--- nostalgia/sources/apple/storesactivity.py
+++ nostalgia/sources/apple/storesactivity.py
@@ -7,7 +7,7 @@ import os
 class StoresActivity(NDF):
     @classmethod
     def load(cls, nrows=None):
-        files = os.path.join("~/nostalgia_data/input/apple/*/Apple_Media_Services/Stores Activity/Account and Transaction History/Store*History.csv")
+        files = "~/nostalgia_data/input/apple/*/Apple_Media_Services/Stores Activity/Account and Transaction History/Store*History.csv"
         dfs = [pd.read_csv(f, parse_dates=["Item Purchased Date"]) for f in just.glob(files)]
         applications = pd.merge(dfs[0], dfs[1], how="outer", on="Item Purchased Date")
         return cls(applications)
