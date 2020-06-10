c6bcb14f820d333bc4d5ad2953acdb6116be51e2
André Jaenisch <andre.jaenisch@posteo.de>
2020-06-10 22:57:50 +0200

    Iterate over dict instead of creating classmethods

--- nostalgia/sources/twitter/__init__.py
+++ nostalgia/sources/twitter/__init__.py
@@ -5,6 +5,32 @@ import just
 from nostalgia.ndf import NDF
 
 
+file_mappings = [{
+    "from_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/ad-engagements.js"),
+    "to_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/ad-engagements.json")
+}, {
+    "from_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/connected-application.js"),
+    "to_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/connected-application.json")
+}, {
+    "from_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/direct-messages.js"),
+    "to_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/direct-messages.json")
+}, {
+    "from_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/device-token.js"),
+    "to_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/device-token.json")
+}, {
+    "from_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/email-address-change.js"),
+    "to_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/email-address-change.json")
+}, {
+    "from_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/ip-audit.js"),
+    "to_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/ip-audit.json")
+}, {
+    "from_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/like.js"),
+    "to_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/like.json")
+}, {
+    "from_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/tweet.js"),
+    "to_path": os.path.expanduser("~/nostalgia_data/input/twitter/data/tweet.json")
+}]
+
 class Twitter(NDF):
     vendor = "twitter"
     ingest_settings = {"ingest_glob": "~/Downloads/twitter-20*-*.zip", "recent_only": False, "delete_existing": False}
@@ -12,62 +38,8 @@ class Twitter(NDF):
     @classmethod
     def ingest(cls):
         super().ingest()
-        Twitter._transform_ad_engagement_source()
-        Twitter._transform_connected_applications_source()
-        Twitter._transform_device_token_source()
-        Twitter._transform_dm_source()
-        Twitter._transform_email_changed_source()
-        Twitter._transform_ip_audit_source()
-        Twitter._transform_like_source()
-        Twitter._transform_tweet_source()
-
-    @classmethod
-    def _transform_ad_engagement_source(cls):
-        from_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/ad-engagements.js")
-        to_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/ad-engagements.json")
-        Twitter._transform_js_file(from_path, to_path)
-
-    @classmethod
-    def _transform_connected_applications_source(cls):
-        from_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/connected-application.js")
-        to_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/connected-application.json")
-        Twitter._transform_js_file(from_path, to_path)
-
-    @classmethod
-    def _transform_dm_source(cls):
-        from_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/direct-messages.js")
-        to_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/direct-messages.json")
-        Twitter._transform_js_file(from_path, to_path)
-
-    @classmethod
-    def _transform_device_token_source(cls):
-        from_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/device-token.js")
-        to_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/device-token.json")
-        Twitter._transform_js_file(from_path, to_path)
-
-    @classmethod
-    def _transform_email_changed_source(cls):
-        from_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/email-address-change.js")
-        to_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/email-address-change.json")
-        Twitter._transform_js_file(from_path, to_path)
-
-    @classmethod
-    def _transform_ip_audit_source(cls):
-        from_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/ip-audit.js")
-        to_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/ip-audit.json")
-        Twitter._transform_js_file(from_path, to_path)
-
-    @classmethod
-    def _transform_like_source(cls):
-        from_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/like.js")
-        to_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/like.json")
-        Twitter._transform_js_file(from_path, to_path)
-
-    @classmethod
-    def _transform_tweet_source(cls):
-        from_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/tweet.js")
-        to_path = os.path.expanduser("~/nostalgia_data/input/twitter/data/tweet.json")
-        Twitter._transform_js_file(from_path, to_path)
+        for mapping in file_mappings:
+            cls._transform_js_file(mapping["from_path"], mapping["to_path"])
 
     @classmethod
     def _transform_js_file(cls, from_path, to_path):
